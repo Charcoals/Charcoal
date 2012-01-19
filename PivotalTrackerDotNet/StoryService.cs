5cf47490c2c14a7c6a80e76da5ad94f2d8b83b4e
v5 dev <ngoede@algorithmics.com>
2012-01-19 13:55:45 -0500

    Fixed ref errors

--- PivotalTrackerDotNet/StoryService.cs
+++ PivotalTrackerDotNet/StoryService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using PivotalTrackerDotNet.Domain;
 using RestSharp;
-using RestSharp.Authenticators;
 
 namespace PivotalTrackerDotNet {
 	public class StoryService : AAuthenticatedService {
