c2603b12f26565b963aa83347fb08cde536e8cae
V5 Core Dev <collv5coredev@algorithmics.com>
2012-01-24 11:28:49 -0500

    Moving stories into their own controls and styling

---
+++ PivotalExtension.TaskManager/Models/StoryRowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PivotalTrackerDotNet.Domain;
+
+namespace PivotalExtension.TaskManager.Models {
+	public class StoryRowViewModel {
+
+		public StoryRowViewModel(Story story) {
+			Story = story;
+		}
+
+		public Story Story { get; set; }
+	}
+}
---
+++ PivotalExtension.TaskManager/Models/StoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PivotalTrackerDotNet.Domain;
+
+namespace PivotalExtension.TaskManager.Models {
+	public class StoryViewModel {
+
+		public StoryViewModel(Story story) {
+			Story = story;
+		}
+
+		public Story Story { get; set; }
+	}
+}
