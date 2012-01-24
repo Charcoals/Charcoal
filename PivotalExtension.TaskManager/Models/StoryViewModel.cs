9f6768d7651e4f54c2b0fd56fc63636cc59d022d
V5 Core Dev <collv5coredev@algorithmics.com>
2012-01-24 16:07:21 -0500

    - Changing styling - Adding checkbox for hiding stories - Show story type

--- PivotalExtension.TaskManager/Models/StoryViewModel.cs
+++ PivotalExtension.TaskManager/Models/StoryViewModel.cs
@@ -12,5 +12,25 @@ namespace PivotalExtension.TaskManager.Models {
 		public string GetStyle() {
 			return "story-details " + Story.CurrentState;
 		}
+
+		public string GetHeader() {
+			if (Story.StoryType == "bug") {
+				return "Bug";
+			}
+
+			if (Story.StoryType == "chore") {
+				return "Spike / Misc";
+			}
+
+			return "Story";
+		}
+
+		public string GetEstimate() {
+			if(Story.Estimate > 0) {
+				return Story.Estimate + " points";
+			}
+
+			return string.Empty;
+		}
 	}
 }
