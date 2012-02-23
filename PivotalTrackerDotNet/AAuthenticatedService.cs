4410340f5b5f4c9d95fd6b602f161e09ade12cd1
Alex Ullrich <alex.ullrich@gmail.com>
2012-02-23 08:47:15 -0500

    being a pain in the ass about formatting

--- PivotalTrackerDotNet/AAuthenticatedService.cs
+++ PivotalTrackerDotNet/AAuthenticatedService.cs
@@ -2,43 +2,41 @@
 using RestSharp;
 
 namespace PivotalTrackerDotNet {
-	public abstract class AAuthenticatedService {
-		protected readonly AuthenticationToken m_token;
-		protected RestClient RestClient;
-		protected AAuthenticatedService(AuthenticationToken token) {
-			m_token = token;
-			RestClient = new RestClient();
-			RestClient.BaseUrl = PivotalTrackerRestEndpoint.ENDPOINT;
-		}
+    public abstract class AAuthenticatedService {
+        protected readonly AuthenticationToken m_token;
+        protected RestClient RestClient;
+        protected AAuthenticatedService(AuthenticationToken token) {
+            m_token = token;
+            RestClient = new RestClient();
+            RestClient.BaseUrl = PivotalTrackerRestEndpoint.ENDPOINT;
+        }
 
-		protected RestRequest BuildGetRequest() {
-			var request = new RestRequest(Method.GET);
-			request.AddHeader("X-TrackerToken", m_token.Guid.ToString("N"));
-			request.RequestFormat = DataFormat.Xml;
-			return request;
-		}
+        protected RestRequest BuildGetRequest() {
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("X-TrackerToken", m_token.Guid.ToString("N"));
+            request.RequestFormat = DataFormat.Xml;
+            return request;
+        }
 
-		protected RestRequest BuildPutRequest() {
-			var request = new RestRequest(Method.PUT);
-			request.AddHeader("X-TrackerToken", m_token.Guid.ToString("N"));
-			request.RequestFormat = DataFormat.Xml;
-			return request;
-		}
+        protected RestRequest BuildPutRequest() {
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("X-TrackerToken", m_token.Guid.ToString("N"));
+            request.RequestFormat = DataFormat.Xml;
+            return request;
+        }
 
-        protected RestRequest BuildDeleteRequest()
-        {
+        protected RestRequest BuildDeleteRequest() {
             var request = new RestRequest(Method.DELETE);
             request.AddHeader("X-TrackerToken", m_token.Guid.ToString("N"));
             request.RequestFormat = DataFormat.Xml;
             return request;
         }
 
-        protected RestRequest BuildPostRequest()
-        {
+        protected RestRequest BuildPostRequest() {
             var request = new RestRequest(Method.POST);
             request.AddHeader("X-TrackerToken", m_token.Guid.ToString("N"));
             request.RequestFormat = DataFormat.Xml;
             return request;
         }
-	}
+    }
 }
--- PivotalTrackerDotNet/PivotalTrackerRestEndpoint.cs
+++ PivotalTrackerDotNet/PivotalTrackerRestEndpoint.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PivotalTrackerDotNet
-{
-    public class PivotalTrackerRestEndpoint
-    {
+namespace PivotalTrackerDotNet {
+    public class PivotalTrackerRestEndpoint {
         public const string ENDPOINT = "https://www.pivotaltracker.com/services/v3";
     }
 }
