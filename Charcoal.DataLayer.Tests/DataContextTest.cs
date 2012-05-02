71e187116a7aebb2e13103a2fcba6501bdec5637
Alex Ullrich <alex.ullrich@gmail.com>
2012-05-01 22:00:25 -0400

    messing with querying for child objects in raven

--- Charcoal.DataLayer.Tests/DataContextTest.cs
+++ Charcoal.DataLayer.Tests/DataContextTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Charcoal.Core;
 using NUnit.Framework;
@@ -10,8 +11,7 @@ namespace Charcoal.DataLayer.Tests {
         private ISessionProvider provider = new SessionProvider();
 
         [Test]
-        public void CanSaveSomething()
-        {
+        public void CanSaveSomething() {
             var session = provider.Session();
 
             var context = new DataContext(session);
@@ -34,6 +34,46 @@ namespace Charcoal.DataLayer.Tests {
 
             var loaded = context.Load<Story>(expectedKey);
             Assert.NotNull(loaded);
+        }
+
+        [Test]
+        public void CanQueryForChildObject() {
+            //this kinda sucks
+            var session = provider.Session();
+
+            var context = new DataContext(session);
+
+            var id = Guid.NewGuid();
+            var taskid = Guid.NewGuid();
+
+            var expectedKey = "stories/" + id;
+            var expectedTaskId = "tasks/" + taskid;
+
+            var taskDescription = "asdfasdfasdf";
+
+            var story = new Story();
+            story.SetId(id);
+            story.ETag = Guid.NewGuid();
+            story.Description = "some text";
+            story.Tasks = new List<Task> { new Task { Description = taskDescription, ETag = Guid.NewGuid() }};
+            story.Tasks.First().SetId(taskid);
+
+            Assert.AreEqual(expectedKey, story.Id);
+            Assert.AreEqual(expectedTaskId, story.Tasks.First().Id);
+
+            context.Store(story);
+            context.SaveChanges();
+
+            var queried = context.Query<Story>()
+                .Where(st => st.Tasks.Any(t => t.Id == expectedTaskId))
+                .ToList() //SelectMany not in raven's linq provider
+                .SelectMany(st => st.Tasks)
+                .Where(t => t.Id == expectedTaskId)
+                .ToList();
+
+            Assert.AreEqual(1, queried.Count());
+            Assert.AreEqual(taskDescription, queried.First().Description);
+
         }
     }
 }
